8b775ecf24f7315581791955b6d4657828ab60ae
Mark Carlo V. Garjas <markcarlo859@gmail.com>
2022-11-24 15:04:43 +0800

    validate alias limit number to input and only number can put.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,7 @@
 class Post < ApplicationRecord
   validates :post_long_url, presence: true, format: URI::DEFAULT_PARSER.make_regexp(%w[http https])
   validates :post_short_url, presence: true
-  validates :post_alias, presence: true, uniqueness: true
+  validates :post_alias, presence: true, uniqueness: true, length: { minimum: 4, maximum: 4 }, numericality: { only_integer: true }
 
   belongs_to :user
 
